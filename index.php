7eee2306fa92059b3fb4f4268309c65ada7167e3
samarthhalyal@gmail.com <samarthhalyal@gmail.com>
2019-05-05 00:50:27 +0530

    first push

--- index.php
+++ index.php
@@ -10,62 +10,62 @@ if($method == 'POST'){
   
   switch ($text) {
     case 'Microsoft':
-      $text = "These are information pertaining ".$text." in my database";
-      $text = $text."\nprogramming languages used are : c  c#  python";
+      $text = "These are information pertaining ".$text." in my database,";
+      $text = $text."\nprogramming languages used are : c  c#  python,";
       $text = $text."\nNearest workplace : Bangalore, whitefield";
       break;
     
     case 'Facebook':
-     $text = "These are information pertaining ".$text." in my database";
-      $text = $text."\nprogramming languages used are : php python  java  javascript";
+     $text = "These are information pertaining ".$text." in my database,";
+      $text = $text."\nprogramming languages used are : php python  java  javascript,";
       $text = $text."\nNearest workplace : 1 Hacker Way 94025 Menlo Park, California";
       break;
 
     case 'Mistral Solutions':
-      $text = "These are information pertaining ".$text." in my database";
-      $text = $text."\nprogramming languages used are : c  java  python";
+      $text = "These are information pertaining ".$text." in my database,";
+      $text = $text."\nprogramming languages used are : c  java  python,";
       $text = $text."\nNearest workplace : Bangalore, Domlur";
       break;
 
     case 'Mistral':
-      $text = "These are information pertaining ".$text." in my database";
-      $text = $text."\nprogramming languages used are : c  java  python";
+      $text = "These are information pertaining ".$text." in my database,";
+      $text = $text."\nprogramming languages used are : c  java  python,";
       $text = $text."\nNearest workplace : Bangalore, Domlur";
       break;
 
     case 'Google':
-      $text = "These are information pertaining ".$text." in my database";
-      $text = $text."\nprogramming languages used are : python javascript";
+      $text = "These are information pertaining ".$text." in my database,";
+      $text = $text."\nprogramming languages used are : python javascript,";
       $text = $text."\nNearest workplace : Mumbai";
       break;
 
     case 'tcs':
-      $text = "These are information pertaining ".$text." in my database";
-      $text = $text."\nprogramming languages used are : c  c++  python";
+      $text = "These are information pertaining ".$text." in my database,";
+      $text = $text."\nprogramming languages used are : c  c++  python,";
       $text = $text."\nNearest workplace : Bangalore, whitefield";
       break;
 
     case 'mindtree':
-      $text = "These are information pertaining ".$text." in my database";
-      $text = $text."\nprogramming languages used are : c  c#  python";
+      $text = "These are information pertaining ".$text." in my database,";
+      $text = $text."\nprogramming languages used are : c  c#  python,";
       $text = $text."\nNearest workplace : Bangalore, whitefield";
       break;
 
     case 'mercedes benz':
-      $text = "These are information pertaining ".$text." in my database";
-      $text = $text."\nprogramming languages used are : python java";
+      $text = "These are information pertaining ".$text." in my database,";
+      $text = $text."\nprogramming languages used are : python java,";
       $text = $text."\nNearest workplace : Bangalore, whitefield";
       break;
 
     case 'mercedes':
-      $text = "These are information pertaining ".$text." in my database";
-      $text = $text."\nprogramming languages used are : python java";
+      $text = "These are information pertaining ".$text." in my database,";
+      $text = $text."\nprogramming languages used are : python java,";
       $text = $text."\nNearest workplace : Bangalore, whitefield";
       break;
 
     default:
-      $text = "These are information pertaining ".$text." in my database";
-      $text = $text."\nprogramming languages used are : c  c++  python";
+      $text = "These are information pertaining ".$text." in my database,";
+      $text = $text."\nprogramming languages used are : c  c++  python,";
       $text = $text."\nNearest workplace : Delhi, IT road 435";
       break;
   }
