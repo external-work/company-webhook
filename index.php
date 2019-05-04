f4b0947b146f2a57f64efb46e4920af2b6bb4503
samarthhalyal@gmail.com <samarthhalyal@gmail.com>
2019-05-04 23:43:14 +0530

    first push

--- index.php
+++ index.php
@@ -30,10 +30,47 @@ if($method == 'POST'){
 	$response = new \stdClass();
 	// $response->queryResult->fulfillmentText = "Hey! Sam, response from webhook";
 	$response->responseId = $json->responseId;
-	$response->queryResult = $json->queryResult;
+	// $response->queryResult = $json->queryResult;
+	$response->queryResult = json_encode({
+    "queryText": "can you tell me something about Mistral Solutions",
+    "parameters": {
+      "Company": "Mistral Solutions"
+    },
+    "allRequiredParamsPresent": true,
+    "fulfillmentText": "Hey! Sam, response from webhook",
+    "fulfillmentMessages": [
+      {
+        "platform": "ACTIONS_ON_GOOGLE",
+        "simpleResponses": {
+          "simpleResponses": [
+            {
+              "textToSpeech": "wait a bit, ill check to see if i have something in my database!"
+            }
+          ]
+        }
+      },
+      {
+        "text": {
+          "text": [
+            "wait a bit, ill check to see if i have something in my database!"
+          ]
+        }
+      }
+    ],
+    "intent": {
+      "name": "projects/company-f811e/agent/intents/90be1d8d-492d-4087-b8cb-624f5bd8c9bf",
+      "displayName": "Company"
+    },
+    "intentDetectionConfidence": 0.5830773,
+    "diagnosticInfo": {
+      "webhook_latency_ms": 302
+    },
+    "languageCode": "en"
+  });
 	$response->webhookStatus->message = "Samarth is great";
 	// $response->queryResult->fulfillmentText = "Hey! Sam, response from webhook";
-	$response['queryResult']['fulfillmentText'] = "Hey! Sam, response from webhook";
+	// $response["queryResult"]["fulfillmentText"] = "Hey! Sam, response from webhook";
+	// $response["queryResult"]["fulfillmentMessages"] = "Hey! Sam, response from webhook";
 	echo json_encode($response);
 }
 else
