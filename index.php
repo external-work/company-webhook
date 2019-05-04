a3048e3ea6a53022bd28245d65e20f783a3291b9
samarthhalyal@gmail.com <samarthhalyal@gmail.com>
2019-05-04 22:42:51 +0530

    first push

--- index.php
+++ index.php
@@ -9,28 +9,67 @@ if($method == 'POST'){
 
 	$text = $json->queryResult->parameters->Company;
 
-	switch ($text) {
-		case 'hi':
-			$speech = "Hi, Nice to meet you";
-			break;
+	// switch ($text) {
+	// 	case 'hi':
+	// 		$speech = "Hi, Nice to meet you";
+	// 		break;
 
-		case 'bye':
-			$speech = "Bye, good night";
-			break;
+	// 	case 'bye':
+	// 		$speech = "Bye, good night";
+	// 		break;
 
-		case 'anything':
-			$speech = "Yes, you can type anything here.";
-			break;
+	// 	case 'anything':
+	// 		$speech = "Yes, you can type anything here.";
+	// 		break;
 		
-		default:
-			$speech = "Sorry, I didnt get that. Please ask me something else.";
-			break;
-	}
+	// 	default:
+	// 		$speech = "Sorry, I didnt get that. Please ask me something else.";
+	// 		break;
+	// }
 
-	$response = new \stdClass();
-	$response->speech = $speech;
-	$response->displayText = $speech;
-	$response->source = "webhook";
+	$response = {
+  "responseId": "96620af9-b7e0-441a-8318-82d41afa513e-32138632",
+  "queryResult": {
+    "queryText": "can you tell me something about Mistral Solutions",
+    "parameters": {
+      "Company": "Mistral Solutions"
+    },
+    "allRequiredParamsPresent": true,
+    "fulfillmentText": "Hey Company name i recieved",
+    "fulfillmentMessages": [
+      {
+        "platform": "ACTIONS_ON_GOOGLE",
+        "simpleResponses": {
+          "simpleResponses": [
+            {
+              "textToSpeech": "Hey Company name i recieved"
+            }
+          ]
+        }
+      },
+      {
+        "text": {
+          "text": [
+            "Hey Company name i recieved"
+          ]
+        }
+      }
+    ],
+    "webhookSource": "webhook",
+    "intent": {
+      "name": "projects/company-f811e/agent/intents/90be1d8d-492d-4087-b8cb-624f5bd8c9bf",
+      "displayName": "Company"
+    },
+    "intentDetectionConfidence": 0.5742474,
+    "diagnosticInfo": {
+      "webhook_latency_ms": 299
+    },
+    "languageCode": "en"
+  },
+  "webhookStatus": {
+    "message": "Webhook execution successful"
+  }
+}
 	echo json_encode($response);
 }
 else
